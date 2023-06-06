73cecdc053d7eed48c80f0f22e6660da2f061604
I WAYAN RAKA PUTRA YANA <rakaputray20@gmail.com>
2023-06-06 10:04:45 +0800

    added berita.html / working on it

--- style/js/style.js
+++ style/js/style.js
@@ -1,6 +1,15 @@
+// $document().ready(function(){
+
+//     $("a").click(function(){
+//         $("#home").load("index.html");
+//     })
+
+// });
+
 //call the nav and footer
 $(function(){
     $("#navbar").load("navbar.html");
     $("#footer").load("footer.html");
 });
+
 
